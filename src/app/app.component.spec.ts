05e0d2fdddcd85ac72fdbc43eb61ef1d12ca880a
deegeemee <me@deegeemee.net>
2024-03-16 11:37:11 +0100

    Update form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,77 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormControl, FormGroup } from '@angular/forms';
 
 describe('AppComponent', () => {
+  // The fixture of the component
+  // Gets set after the component is created
+  let fix: ComponentFixture<AppComponent>;
+
+  // The instance of the component
+  // Gets set after the component is created
+  let ci: AppComponent;
+
+  // The debugElement of the component
+  // Gets set after the component is created
+  let de: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    fix = TestBed.createComponent(AppComponent);
+    ci = fix.componentInstance;
+    de = fix.debugElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(ci).toBeTruthy();
   });
 
   it(`should have the 'reticle-generator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('reticle-generator');
+    expect(ci.title).toEqual('reticle-generator');
+  });
+
+  it(`should a .svg div`, () => {
+    expect(de.query(By.css('.svg'))).toBeTruthy();
+  });
+
+  it(`should a .form div`, () => {
+    expect(de.query(By.css('.form'))).toBeTruthy();
+  });
+
+
+  describe('form', () => {
+    it(`should be defined as a FormGroup`, () => {
+      expect(ci.form).toBeInstanceOf(FormGroup);
+    });
+
+    it(`should have a size FormControl`, () => {
+      expect(ci.form.controls.size).toBeInstanceOf(FormControl);
+    });
+
+    it(`should have a lineWidth FormControl`, () => {
+      expect(ci.form.controls.lineWidth).toBeInstanceOf(FormControl);
+    });
+
+    it(`should have a offsetLeft FormControl`, () => {
+      expect(ci.form.controls.offsetLeft).toBeInstanceOf(FormControl);
+    });
+
+    it(`should have a offsetRight FormControl`, () => {
+      expect(ci.form.controls.offsetRight).toBeInstanceOf(FormControl);
+    });
+
+    it(`should have a offsetTop FormControl`, () => {
+      expect(ci.form.controls.offsetTop).toBeInstanceOf(FormControl);
+    });
+
+    it(`should have a offsetBottom FormControl`, () => {
+      expect(ci.form.controls.offsetBottom).toBeInstanceOf(FormControl);
+    });
+
   });
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,89 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material.module';
+
+type AxisSectionFormType = {
+  angle: FormControl<number | null>;
+  enabled: FormControl<boolean | null>;
+  count: FormControl<number | null>;
+  width: FormControl<number | null>;
+  offset: FormControl<number | null>;
+  size: FormControl<number | null>;
+};
+
+type AxisFormType = {
+  enabled: FormControl<boolean | null>;
+  line: FormControl<boolean | null>;
+  sections: FormArray<FormGroup<AxisSectionFormType>>;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, MaterialModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reticle-generator';
 
   form = new FormGroup({
-    size: new FormControl<number>(1024, { validators: [Validators.min(512), Validators.max(4096)] }),
-    lineWidth: new FormControl<number>(1, { validators: [Validators.min(512), Validators.max(4096)] }),
-    offsetLeft: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
-    offsetRight: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
-    offsetTop: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
-    offsetBottom: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
-    horEndingSize: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
-    vertEndingSize: new FormControl<number>(0, { validators: [Validators.min(512), Validators.max(4096)] }),
+    size: new FormControl<number>(1024, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    lineWidth: new FormControl<number>(1, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    offsetLeft: new FormControl<number>(0, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    offsetRight: new FormControl<number>(0, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    offsetTop: new FormControl<number>(0, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    offsetBottom: new FormControl<number>(0, {
+      validators: [Validators.min(512), Validators.max(4096)],
+    }),
+    axis: new FormArray<FormGroup<AxisFormType>>([]),
   });
 
+  /**
+   * Return for group for an axis
+   * @returns FormGroup<AxisFormType>
+   */
+  private getAxisForm(): FormGroup<AxisFormType> {
+    return new FormGroup({
+      enabled: new FormControl<boolean>(true),
+      line: new FormControl<boolean>(true),
+      sections: new FormArray([
+        this.getAxisSectionForm(),
+      ]),
+    });
+  }
+
+  /**
+   * Return for group for an axis section
+   * @returns FormGroup<AxisSectionFormType>
+   */
+  private getAxisSectionForm(): FormGroup<AxisSectionFormType> {
+    return new FormGroup<AxisSectionFormType>({
+      angle: new FormControl<number>(0, { validators: [Validators.min(0), Validators.max(360)] }),
+      enabled: new FormControl<boolean>(true),
+      count: new FormControl<number>(0, {
+        validators: [Validators.min(0), Validators.max(10)],
+      }),
+      width: new FormControl<number>(1, {
+        validators: [Validators.min(1), Validators.max(10)],
+      }),
+      offset: new FormControl<number>(5, {
+        validators: [Validators.min(0), Validators.max(10)],
+      }),
+      size: new FormControl<number>(10, {
+        validators: [Validators.min(0), Validators.max(10)],
+      }),
+    });
+  }
 }
