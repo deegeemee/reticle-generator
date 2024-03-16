083507dd1d46c710cfcbb89e79833a442865d4f9
deegeemee <me@deegeemee.net>
2024-03-16 16:17:38 +0100

    Add more material components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,14 @@
 // Angular module for material components
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   exports: [
@@ -17,6 +19,8 @@ import { MatRadioModule } from '@angular/material/radio';
     MatCheckboxModule,
     MatDividerModule,
     MatRadioModule,
+    MatSlideToggleModule,
+    MatIconModule,
   ],
 })
 export class MaterialModule {}
