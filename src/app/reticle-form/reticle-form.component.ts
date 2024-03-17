a72b4ce83a9bfc4cccd5cd66dd4f8487a6654204
deegeemee <me@deegeemee.net>
2024-03-17 16:07:42 +0100

    Apply limits to form fields

--- src/app/reticle-form/reticle-form.component.ts
+++ src/app/reticle-form/reticle-form.component.ts
@@ -6,6 +6,14 @@ import { AutoForm } from '../form.types';
 import { MaterialModule } from '../material.module';
 import { AxisSectionType, AxisType, ReticleType } from '../reticle.types';
 
+export type NumberFormElementSettings<T> = {
+  [P in keyof T]: {
+    default: number;
+    min: number;
+    max: number;
+  };
+};
+
 @Component({
   selector: 'app-reticle-form',
   standalone: true,
@@ -14,6 +22,21 @@ import { AxisSectionType, AxisType, ReticleType } from '../reticle.types';
   styleUrl: './reticle-form.component.css',
 })
 export class ReticleFormComponent {
+  readonly axisFormSettings: NumberFormElementSettings<Omit<AxisType, 'sections' | 'enabled'>> = {
+    angle: { default: 0, min: 0, max: 360 },
+    offsetStart: { default: 256, min: 0, max: 512 },
+    offsetEnd: { default: 256, min: 0, max: 512 },
+    strokeWidth: { default: 3, min: 1, max: 20 },
+  };
+
+  readonly axisSectionFormSettings: NumberFormElementSettings<Omit<AxisSectionType, 'enabled'>> = {
+    count: { default: 0, min: 0, max: 10 },
+    gap: { default: 20, min: 1, max: 100 },
+    offset: { default: -10, min: -100, max: 100 },
+    length: { default: 20, min: 0, max: 200 },
+    strokeWidth: { default: 1, min: 1, max: 20 },
+  };
+
   readonly form: AutoForm<ReticleType> = new FormGroup({
     size: new FormControl<number>(1024, {
       validators: [Validators.min(512), Validators.max(4096)],
@@ -37,20 +60,29 @@ export class ReticleFormComponent {
   getAxisForm(value?: AxisType): AutoForm<AxisType> {
     const fg: AutoForm<AxisType> = new FormGroup({
       enabled: new FormControl<boolean>(true, { nonNullable: true }),
-      angle: new FormControl<number>(0, {
-        validators: [Validators.min(0), Validators.max(360)],
+      angle: new FormControl<number>(this.axisFormSettings.angle.default, {
+        validators: [Validators.min(this.axisFormSettings.angle.min), Validators.max(this.axisFormSettings.angle.max)],
         nonNullable: true,
       }),
-      offsetStart: new FormControl<number>(256, {
-        validators: [Validators.min(0), Validators.max(360)],
+      offsetStart: new FormControl<number>(this.axisFormSettings.offsetStart.default, {
+        validators: [
+          Validators.min(this.axisFormSettings.offsetStart.min),
+          Validators.max(this.axisFormSettings.offsetStart.max),
+        ],
         nonNullable: true,
       }),
-      offsetEnd: new FormControl<number>(256, {
-        validators: [Validators.min(0), Validators.max(360)],
+      offsetEnd: new FormControl<number>(this.axisFormSettings.offsetStart.default, {
+        validators: [
+          Validators.min(this.axisFormSettings.offsetStart.min),
+          Validators.max(this.axisFormSettings.offsetStart.max),
+        ],
         nonNullable: true,
       }),
-      strokeWidth: new FormControl<number>(3, {
-        validators: [Validators.min(0), Validators.max(360)],
+      strokeWidth: new FormControl<number>(this.axisFormSettings.strokeWidth.default, {
+        validators: [
+          Validators.min(this.axisFormSettings.strokeWidth.min),
+          Validators.max(this.axisFormSettings.strokeWidth.max),
+        ],
         nonNullable: true,
       }),
       sections: new FormArray(
@@ -72,24 +104,39 @@ export class ReticleFormComponent {
   getAxisSectionForm(value?: AxisSectionType): AutoForm<AxisSectionType> {
     const fg: AutoForm<AxisSectionType> = new FormGroup({
       enabled: new FormControl<boolean>(true, { nonNullable: true }),
-      count: new FormControl<number>(0, {
-        validators: [Validators.min(0), Validators.max(10)],
+      count: new FormControl<number>(this.axisSectionFormSettings.count.default, {
+        validators: [
+          Validators.min(this.axisSectionFormSettings.count.min),
+          Validators.max(this.axisSectionFormSettings.count.max),
+        ],
         nonNullable: true,
       }),
-      gap: new FormControl<number>(20, {
-        validators: [Validators.min(1), Validators.max(100)],
+      gap: new FormControl<number>(this.axisSectionFormSettings.gap.default, {
+        validators: [
+          Validators.min(this.axisSectionFormSettings.gap.min),
+          Validators.max(this.axisSectionFormSettings.gap.max),
+        ],
         nonNullable: true,
       }),
-      offset: new FormControl<number>(-10, {
-        validators: [Validators.min(-100), Validators.max(100)],
+      offset: new FormControl<number>(this.axisSectionFormSettings.offset.default, {
+        validators: [
+          Validators.min(this.axisSectionFormSettings.offset.min),
+          Validators.max(this.axisSectionFormSettings.offset.max),
+        ],
         nonNullable: true,
       }),
-      length: new FormControl<number>(20, {
-        validators: [Validators.min(0), Validators.max(200)],
+      length: new FormControl<number>(this.axisSectionFormSettings.length.default, {
+        validators: [
+          Validators.min(this.axisSectionFormSettings.length.min),
+          Validators.max(this.axisSectionFormSettings.length.max),
+        ],
         nonNullable: true,
       }),
-      strokeWidth: new FormControl<number>(1, {
-        validators: [Validators.min(1), Validators.max(20)],
+      strokeWidth: new FormControl<number>(this.axisSectionFormSettings.strokeWidth.default, {
+        validators: [
+          Validators.min(this.axisSectionFormSettings.strokeWidth.min),
+          Validators.max(this.axisSectionFormSettings.strokeWidth.max),
+        ],
         nonNullable: true,
       }),
     });
