9d800ce6887ddeb27b13b6f55d21c1bf1b176df5
deegeemee <me@deegeemee.net>
2024-07-01 20:36:40 +0200

    release: v0.0.2

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,11 @@ import { RuleConfigSeverity } from '@commitlint/types';
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'release'],
+    ],
     'body-max-line-length': [RuleConfigSeverity.Error, 'always', 512] as const,
   },
 };
